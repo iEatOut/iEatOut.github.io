8e15377cb4f6b3f4d632d96e0c1373a3def3085e
Shuyang Chen <sc1085@gmail.com>
2014-11-16 01:48:06 -0500

    Cleaned up some html and removed unnecessary javascript function

--- js/main.js
+++ js/main.js
@@ -7,43 +7,6 @@ var $$ = Dom7;
 
 var mainView = fw.addView('#ieo-view', { dynamicNavbar: true });
 
-// Callbacks to run specific code for specific pages, for example for About page:
-fw.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
-});
-
-// Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}
-
 function popup(title, text, target) {
     $('#ieo-popover-title').text(title);
     $('#ieo-popover-text').text(text);
@@ -52,7 +15,7 @@ function popup(title, text, target) {
 
 $('#ieo-login-submit').click(function (e) {
    fb.authWithPassword({
-       email    : $('#ieo-login-email').val(),
+       email : $('#ieo-login-email').val(),
        password : $('#ieo-login-pass').val()
    }, function(error, authData) {
        if (error) {
@@ -71,7 +34,7 @@ $$(document).on('pageAfterAnimation', function (e) {
 
 $('#ieo-register-submit').click(function (e) {
     fb.createUser({
-        email    : $('#ieo-register-email').val(),
+        email : $('#ieo-register-email').val(),
         password : $('#ieo-register-pass').val()
     }, function(error) {
         if (error) {
